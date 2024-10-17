2f8edfad9e49f42908787ba980bb7d2dc84461d5
BusySnow <thanglb.bi11-243@st.usth.edu.vn>
2024-10-17 23:10:26 +0700

    add: day 14

---
+++ Day 14/script.js
@@ -0,0 +1,61 @@
+var imgFeature = document.querySelector(".img-feature");
+var listImg = document.querySelectorAll(".list-image img");
+var prevBtn = document.querySelector(".prev");
+var nextBtn = document.querySelector(".next");
+
+var currentIndex = 0;
+
+function updateImageByIndex(index) {
+  //remove active class
+  document.querySelectorAll(".list-image div").forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  currentIndex = index;
+  imgFeature.style.opacity = "0";
+  setTimeout(() => {
+    imgFeature.src = listImg[index].getAttribute("src");
+    imgFeature.style.opacity = "1";
+  }, 200);
+  listImg[index].parentElement.classList.add("active");
+
+  //hide button if out of album
+  //   if (currentIndex === 0) {
+  //     prevBtn.classList.add("hide");
+  //   } else {
+  //     prevBtn.classList.remove("hide");
+  //   }
+  //   if (currentIndex >= listImg.length - 1) {
+  //     nextBtn.classList.add("hide");
+  //   } else {
+  //     nextBtn.classList.remove("hide");
+  //   }
+}
+
+listImg.forEach((imgElement, index) => {
+  imgElement.addEventListener("click", (e) => {
+    updateImageByIndex(index);
+  });
+});
+
+prevBtn.addEventListener("click", (e) => {
+  if (currentIndex !== 0) {
+    currentIndex--;
+  } else {
+    currentIndex = listImg.length - 1;
+  }
+
+  updateImageByIndex(currentIndex);
+});
+
+nextBtn.addEventListener("click", (e) => {
+  if (currentIndex < listImg.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+
+  updateImageByIndex(currentIndex);
+});
+
+updateImageByIndex(0);
